a8462994cd0baf1631cf27118b966a3db11664b5
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-03-06 12:58:19 +0100

    PokemonDetail: Test for Pokemon Repository added

---
+++ src/features/pokemonDetail/data/repositories/tests/PokemonRepository.test.ts
@@ -0,0 +1,23 @@
+import { PokemonRepository } from '../PokemonRepository';
+
+describe('PokemonDetail: PokemonRepository - E2E test', () => {
+  const repository = new PokemonRepository();
+
+  it('Should fetch the details of a Pokemon', async () => {
+    const pokemon = await repository.getPokemonDetail(1);
+
+    expect(pokemon.id).toBe(1);
+    expect(pokemon.name).toBe('bulbasaur');
+
+    expect(typeof pokemon.image).toBe('string');
+    expect(pokemon.image).toContain('http');
+
+    expect(pokemon.types).toContain('grass');
+    expect(pokemon.types).toContain('poison');
+    expect(pokemon.stats.length).toBeGreaterThan(0);
+    expect(pokemon.abilities.length).toBeGreaterThan(0);
+
+    expect(pokemon.stats[0]).toHaveProperty('name');
+    expect(pokemon.stats[0]).toHaveProperty('value');
+  });
+});
