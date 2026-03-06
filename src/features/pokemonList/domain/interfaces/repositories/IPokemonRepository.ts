d518c1f10726dd4b2e4dc05f85edfccb9e847d0d
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-03-06 11:57:03 +0100

    PokemonList feature: Interface for Pokemon Repository added

---
+++ src/features/pokemonList/domain/interfaces/repositories/IPokemonRepository.ts
@@ -0,0 +1,8 @@
+import { PokemonListResponse } from '@/src/shared/data/api/PokemonListResponse';
+
+export interface IPokemonRepository {
+  // Limit indicates the number of Pokemnon to be fetched, and offset indicates the number of Pokemon to be skipped, so when we want
+  // to fetch the next batch of 30 Pokemon, we can skip the ones we already have
+  getPokemonList(limit: number, offset: number): Promise<PokemonListResponse>;
+  getPokemonTypes(): Promise<string[]>;
+}
