56efc60403eeb988f128140265b1c0ee2ec610f6
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-03-09 18:27:09 +0100

    IPokemonListRepository.ts: Fixed minor spelling mistake in comment

--- src/features/pokemonList/domain/interfaces/repositories/IPokemonListRepository.ts
+++ src/features/pokemonList/domain/interfaces/repositories/IPokemonListRepository.ts
@@ -1,7 +1,7 @@
 import { PokemonListResponse } from '@/src/shared/data/api/PokemonListResponse';
 
 export interface IPokemonListRepository {
-  // Limit indicates the number of Pokemnon to be fetched, and offset indicates the number of Pokemon to be skipped, so when we want
+  // Limit indicates the number of Pokemon to be fetched, and offset indicates the number of Pokemon to be skipped, so when we want
   // to fetch the next batch of 30 Pokemon, we can skip the ones we already have
   getPokemonList(limit: number, offset: number): Promise<PokemonListResponse>;
   getPokemonTypes(): Promise<string[]>;
