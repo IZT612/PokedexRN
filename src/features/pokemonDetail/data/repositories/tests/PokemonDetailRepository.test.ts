d77557fa61ea29096be4f524fc26af7f608213ee
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-03-09 18:00:10 +0100

    Added jest mocks to all tests and added error tests

--- src/features/pokemonDetail/data/repositories/tests/PokemonDetailRepository.test.ts
+++ src/features/pokemonDetail/data/repositories/tests/PokemonDetailRepository.test.ts
@@ -1,9 +1,37 @@
+import apiClient from '@/src/shared/data/api/client';
 import { PokemonDetailRepository } from '../PokemonDetailRepository';
 
-describe('PokemonDetail: PokemonRepository - E2E test', () => {
+jest.mock('@/src/shared/data/api/client');
+
+describe('PokemonDetail: PokemonDetailRepository - E2E test', () => {
   const repository = new PokemonDetailRepository();
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  // Success case: Able to fetch the details of a Pokemon
   it('Should fetch the details of a Pokemon', async () => {
+    const mockApiResponse = {
+      data: {
+        id: 1,
+        name: 'bulbasaur',
+        sprites: {
+          other: {
+            'official-artwork': {
+              front_default:
+                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+            },
+          },
+        },
+        types: [{ type: { name: 'grass' } }, { type: { name: 'poison' } }],
+        stats: [{ base_stat: 45, stat: { name: 'hp' } }],
+        abilities: [{ ability: { name: 'overgrow' } }],
+      },
+    };
+
+    (apiClient.get as jest.Mock).mockResolvedValueOnce(mockApiResponse);
+
     const pokemon = await repository.getPokemonDetail(1);
 
     expect(pokemon.id).toBe(1);
@@ -19,5 +47,30 @@ describe('PokemonDetail: PokemonRepository - E2E test', () => {
 
     expect(pokemon.stats[0]).toHaveProperty('name');
     expect(pokemon.stats[0]).toHaveProperty('value');
+  });
+
+  // Error case: Simulates a network error when trying to fetch the details of a Pokemon
+  it('Should throw a Network error', async () => {
+    const simulatedError = new Error('Network Error');
+    (apiClient.get as jest.Mock).mockRejectedValueOnce(simulatedError);
+
+    await expect(repository.getPokemonDetail(1)).rejects.toThrow(
+      'Network Error',
+    );
+  });
+
+  // Error case: Simulates an error when the API returns invalid data (missing required fields)
+  it('Should throw an error for invalid data', async () => {
+    const invalidData = {
+      data: {
+        id: 1,
+        name: 'bulbasaur',
+        // Missing sprites, types, stats, and abilities
+      },
+    };
+
+    (apiClient.get as jest.Mock).mockResolvedValueOnce(invalidData);
+
+    await expect(repository.getPokemonDetail(1)).rejects.toThrow();
   });
 });
--- src/features/pokemonList/data/repositories/tests/PokemonListRepository.test.ts
+++ src/features/pokemonList/data/repositories/tests/PokemonListRepository.test.ts
@@ -1,41 +1,114 @@
+import apiClient from '@/src/shared/data/api/client';
 import { PokemonListRepository } from '../PokemonListRepository';
 
-describe('PokemonList: PokemonRepository - E2E test', () => {
+jest.mock('@/src/shared/data/api/client');
+
+describe('PokemonList: PokemonListRepository - E2E test', () => {
   const repository = new PokemonListRepository();
 
-  it('Should fetch a list of Pokemon', async () => {
-    const result = await repository.getPokemonList(30, 0);
-
-    expect(result.results.length).toBe(30);
-    expect(typeof result.results[0].name).toBe('string');
-    expect(typeof result.results[0].url).toBe('string');
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  it('Should fetch a list of Pokemon types', async () => {
-    const types = await repository.getPokemonTypes();
+  describe('List tests', () => {
+    // Success case: Able fetch a list of Pokemon
+    it('Should fetch a list of Pokemon', async () => {
+      const mockResults = Array(30).fill({
+        name: 'pikachu',
+        url: 'http://fake-url.com',
+      });
 
-    // There are currently 18 Pokemon types, but we check if it's equal to 18 or greater than 18, because in the future if they add more
-    // types we don't want this test to fail
-    expect(types.length).toBeGreaterThan(18);
+      const mockApiResponse = {
+        data: {
+          results: mockResults,
+          count: 1302,
+          next: 'http://fake-url.com/next',
+          previous: null,
+        },
+      };
 
-    // We check if the types array contains all the currently known Pokemon types
-    expect(types).toContain('normal');
-    expect(types).toContain('fighting');
-    expect(types).toContain('flying');
-    expect(types).toContain('poison');
-    expect(types).toContain('ground');
-    expect(types).toContain('rock');
-    expect(types).toContain('bug');
-    expect(types).toContain('ghost');
-    expect(types).toContain('steel');
-    expect(types).toContain('fire');
-    expect(types).toContain('water');
-    expect(types).toContain('grass');
-    expect(types).toContain('electric');
-    expect(types).toContain('psychic');
-    expect(types).toContain('ice');
-    expect(types).toContain('dragon');
-    expect(types).toContain('dark');
-    expect(types).toContain('fairy');
+      (apiClient.get as jest.Mock).mockResolvedValueOnce(mockApiResponse);
+
+      const result = await repository.getPokemonList(30, 0);
+
+      expect(result.results.length).toBe(30);
+      expect(typeof result.results[0].name).toBe('string');
+      expect(typeof result.results[0].url).toBe('string');
+    });
+
+    // Error case: Simulates a network error when trying to fetch a list of Pokemon
+    it('Should throw a Network error', async () => {
+      const simulatedError = new Error('Network error');
+      (apiClient.get as jest.Mock).mockRejectedValueOnce(simulatedError);
+
+      await expect(repository.getPokemonList(30, 0)).rejects.toThrow(
+        'Network error',
+      );
+    });
+  });
+
+  describe('Types tests', () => {
+    it('Should fetch a list of Pokemon types', async () => {
+      const mockTypesResponse = {
+        data: {
+          results: [
+            { name: 'normal' },
+            { name: 'fighting' },
+            { name: 'flying' },
+            { name: 'poison' },
+            { name: 'ground' },
+            { name: 'rock' },
+            { name: 'bug' },
+            { name: 'ghost' },
+            { name: 'steel' },
+            { name: 'fire' },
+            { name: 'water' },
+            { name: 'grass' },
+            { name: 'electric' },
+            { name: 'psychic' },
+            { name: 'ice' },
+            { name: 'dragon' },
+            { name: 'dark' },
+            { name: 'fairy' },
+          ],
+        },
+      };
+
+      (apiClient.get as jest.Mock).mockResolvedValueOnce(mockTypesResponse);
+
+      const types = await repository.getPokemonTypes();
+
+      // There are currently 18 Pokemon types, but we check if it's equal to 18 or greater than 18, because in the future if they add more
+      // types we don't want this test to fail
+      expect(types.length).toBeGreaterThanOrEqual(18);
+
+      // We check if the types array contains all the currently known Pokemon types
+      expect(types).toContain('normal');
+      expect(types).toContain('fighting');
+      expect(types).toContain('flying');
+      expect(types).toContain('poison');
+      expect(types).toContain('ground');
+      expect(types).toContain('rock');
+      expect(types).toContain('bug');
+      expect(types).toContain('ghost');
+      expect(types).toContain('steel');
+      expect(types).toContain('fire');
+      expect(types).toContain('water');
+      expect(types).toContain('grass');
+      expect(types).toContain('electric');
+      expect(types).toContain('psychic');
+      expect(types).toContain('ice');
+      expect(types).toContain('dragon');
+      expect(types).toContain('dark');
+      expect(types).toContain('fairy');
+    });
+
+    // Error case: Simulates a network error when trying to fetch a list of Pokemon types
+    it('Should throw an error when network fails fetching types', async () => {
+      const simulatedError = new Error('Network Error');
+      (apiClient.get as jest.Mock).mockRejectedValueOnce(simulatedError);
+
+      await expect(repository.getPokemonTypes()).rejects.toThrow();
+    });
   });
 });
