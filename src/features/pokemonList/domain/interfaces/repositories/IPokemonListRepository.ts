9654923db20521cca79a680070c881f4273994ac
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-03-10 14:09:50 +0100

    IPokemonListRepository: Update return for getPokemonTypes()

--- src/features/pokemonList/domain/interfaces/repositories/IPokemonListRepository.ts
+++ src/features/pokemonList/domain/interfaces/repositories/IPokemonListRepository.ts
@@ -1,8 +1,9 @@
 import { PokemonListResponse } from '@/src/shared/data/api/PokemonListResponse';
+import { PokemonType } from '@/src/shared/domain/entities/PokemonType';
 
 export interface IPokemonListRepository {
   // Limit indicates the number of Pokemon to be fetched, and offset indicates the number of Pokemon to be skipped, so when we want
   // to fetch the next batch of 30 Pokemon, we can skip the ones we already have
   getPokemonList(limit: number, offset: number): Promise<PokemonListResponse>;
-  getPokemonTypes(): Promise<string[]>;
+  getPokemonTypes(): Promise<PokemonType[]>;
 }
