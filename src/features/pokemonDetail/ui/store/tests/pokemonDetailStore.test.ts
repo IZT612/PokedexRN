8c4c0ba3efd79b2a9dab4834dc208dd55e914390
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-03-16 15:16:32 +0100

    pokemonDetailStore.test.ts: Add clearPokemonDetail test

--- src/features/pokemonDetail/ui/store/tests/pokemonDetailStore.test.ts
+++ src/features/pokemonDetail/ui/store/tests/pokemonDetailStore.test.ts
@@ -1,7 +1,22 @@
+import { Pokemon } from '@/src/shared/domain/entities/Pokemon';
+import { PokemonType } from '@/src/shared/domain/entities/PokemonType';
 import { usePokemonDetailStore } from '../pokemonDetailStore';
 
 describe('PokemonDetailStore', () => {
   const initialStoreState = usePokemonDetailStore.getState();
+
+  const mockPokemon: Pokemon = {
+    id: 1,
+    name: 'bulbasaur',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+    types: ['grass', 'poison'] as PokemonType[],
+    stats: [
+      { name: 'hp', value: 45 },
+      { name: 'attack', value: 49 },
+    ],
+    abilities: ['overgrow'],
+  };
 
   beforeEach(() => {
     // Reset the store state before each test
@@ -25,5 +40,18 @@ describe('PokemonDetailStore', () => {
     const errorMessage = 'Network Error';
     usePokemonDetailStore.getState().setError(errorMessage);
     expect(usePokemonDetailStore.getState().error).toBe(errorMessage);
+  });
+
+  it('Should clear the pokemon detail correctly', () => {
+    usePokemonDetailStore.setState({
+      pokemonDetail: mockPokemon,
+      error: 'Some error',
+    });
+
+    usePokemonDetailStore.getState().clearPokemonDetail();
+
+    const state = usePokemonDetailStore.getState();
+    expect(state.pokemonDetail).toBeNull();
+    expect(state.error).toBeNull();
   });
 });
