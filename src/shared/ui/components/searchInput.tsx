6e5b0b6226b7c0d86d34569ec58f51d880d7690d
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-03-19 11:18:22 +0100

    Update searchInput.tsx: tamagui added & value as Prop

--- src/shared/ui/components/searchInput.tsx
+++ src/shared/ui/components/searchInput.tsx
@@ -1,38 +1,32 @@
 import React from 'react';
-import { StyleSheet, TextInput, TextInputProps, View } from 'react-native';
+import { TextInputProps } from 'react-native';
+import { ColorTokens, Input } from 'tamagui';
 import {
-    BorderRadius,
-    Colors,
-    Sizes,
-    Spacing,
+  BorderRadius,
+  Colors,
+  Sizes,
+  Spacing,
 } from '../../../../constants/theme';
 
 type Props = {
   placeholder: string;
+  value: string;
   onChangeText: (text: string) => void;
 } & Omit<TextInputProps, 'onChangeText'>;
 
-export const SearchInput = ({ placeholder, onChangeText }: Props) => (
-  <View style={styles.container}>
-    <TextInput
-      style={styles.input}
-      placeholder={placeholder}
-      placeholderTextColor={Colors.light.icon}
-      onChangeText={onChangeText}
-    />
-  </View>
+export const SearchInput = ({ placeholder, value, onChangeText }: Props) => (
+  <Input
+    flex={1}
+    height={Sizes.buttonHeight.md}
+    backgroundColor={Colors.light.surface}
+    borderRadius={BorderRadius.full}
+    paddingHorizontal={Spacing.md}
+    borderWidth={1}
+    borderColor="#E0E0E0"
+    color={Colors.light.text}
+    placeholder={placeholder}
+    placeholderTextColor={Colors.light.icon as ColorTokens}
+    value={value}
+    onChangeText={onChangeText}
+  />
 );
-
-const styles = StyleSheet.create({
-  container: {
-    height: Sizes.buttonHeight.md,
-    backgroundColor: '#F5F5F5',
-    borderRadius: BorderRadius.full,
-    paddingHorizontal: Spacing.md,
-    justifyContent: 'center',
-  },
-  input: {
-    flex: 1,
-    color: Colors.light.text,
-  },
-});
