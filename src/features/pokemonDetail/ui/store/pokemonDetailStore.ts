edb3e1631f84d49f0cd46ded898f269a984273e0
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-03-19 11:23:58 +0100

    Edit pokemonDetail & pokemonList store: Remove devtools & setLoading/setError

--- src/features/pokemonDetail/ui/store/pokemonDetailStore.ts
+++ src/features/pokemonDetail/ui/store/pokemonDetailStore.ts
@@ -1,14 +1,10 @@
-import { Pokemon } from '@/src/shared/domain/entities/Pokemon'; // Ajusta el path si es necesario
+import { Pokemon } from '@/src/shared/domain/entities/Pokemon';
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
 
 export interface PokemonDetailState {
   pokemonDetail: Pokemon | null;
   loading: boolean;
   error: string | null;
-
-  setLoading: (loading: boolean) => void;
-  setError: (error: string | null) => void;
 
   fetchPokemonDetail: (
     idOrName: string | number,
@@ -18,41 +14,22 @@ export interface PokemonDetailState {
   clearPokemonDetail: () => void;
 }
 
-export const usePokemonDetailStore = create<PokemonDetailState>()(
-  devtools(
-    (set) => ({
-      pokemonDetail: null,
-      loading: false,
-      error: null,
+export const usePokemonDetailStore = create<PokemonDetailState>()((set) => ({
+  pokemonDetail: null,
+  loading: false,
+  error: null,
 
-      setLoading: (loading) => set({ loading }, false, 'setLoading'),
-      setError: (error) => set({ error }, false, 'setError'),
+  // Clear the pokemon detail and error state
+  clearPokemonDetail: () => set({ pokemonDetail: null, error: null }),
 
-      // Clear the pokemon detail and error state
-      clearPokemonDetail: () =>
-        set({ pokemonDetail: null, error: null }, false, 'clearPokemonDetail'),
-
-      // Fetch the pokemon detail by id or name (id principally)
-      fetchPokemonDetail: async (idOrName, fetchData) => {
-        set({ loading: true, error: null }, false, 'fetchPokemonDetail');
-        try {
-          const data = await fetchData(idOrName);
-          set(
-            { pokemonDetail: data, loading: false },
-            false,
-            'fetchPokemonDetail/success',
-          );
-        } catch (error) {
-          set(
-            { error: (error as Error).message, loading: false },
-            false,
-            'fetchPokemonDetail/error',
-          );
-        }
-      },
-    }),
-    {
-      name: 'PokemonDetailStore',
-    },
-  ),
-);
+  // Fetch the pokemon detail by id or name (id principally)
+  fetchPokemonDetail: async (idOrName, fetchData) => {
+    set({ loading: true, error: null });
+    try {
+      const data = await fetchData(idOrName);
+      set({ pokemonDetail: data, loading: false });
+    } catch (error) {
+      set({ error: (error as Error).message, loading: false });
+    }
+  },
+}));
--- src/features/pokemonList/ui/store/pokemonListStore.ts
+++ src/features/pokemonList/ui/store/pokemonListStore.ts
@@ -1,6 +1,5 @@
 import { PokemonType } from '@/src/shared/domain/entities/PokemonType';
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
 import { Pokemon } from '../../../../shared/domain/entities/Pokemon';
 
 const BATCH_SIZE = 30;
@@ -36,119 +35,90 @@ export interface PokemonListState {
 
 export const usePokemonListStore = create<PokemonListState>()(
   // Devtools is a middleware that allows us to see the state changes in the browser devtools, it also allows us to time travel and see the state at any point in time.
-  devtools(
-    // The first argument of the devtools function is a function that receives the set function as an argument,
-    // this set function is used to update the state of the store, it takes an object with the new state and an optional boolean to
-    // indicate if we want to replace the state or merge it, and an optional string to indicate the name of the action that is being
-    // performed.
-    (set, get) => ({
-      pokemonList: [],
-      loading: false,
-      error: null,
-      offset: 0,
-      searchQuery: '',
-      selectedType: null,
 
-      setPokemonList: (pokemonList) =>
-        set({ pokemonList }, false, 'setPokemonList'),
-      setLoading: (loading) => set({ loading }, false, 'setLoading'),
-      setError: (error) => set({ error }, false, 'setError'),
-      setSearchQuery: (searchQuery) =>
-        set({ searchQuery }, false, 'setSearchQuery'),
-      setSelectedType: (selectedType) =>
-        set({ selectedType }, false, 'setSelectedType'),
-      clearFilters: () =>
-        set({ searchQuery: '', selectedType: null }, false, 'clearFilters'),
+  // The first argument of the devtools function is a function that receives the set function as an argument,
+  // this set function is used to update the state of the store, it takes an object with the new state and an optional boolean to
+  // indicate if we want to replace the state or merge it, and an optional string to indicate the name of the action that is being
+  // performed.
+  (set, get) => ({
+    pokemonList: [],
+    loading: false,
+    error: null,
+    offset: 0,
+    searchQuery: '',
+    selectedType: null,
 
-      // Gets the filtered list of pokemon based on the search query and selected type
-      getFilteredPokemon: () => {
-        const { pokemonList, searchQuery, selectedType } = get();
+    setPokemonList: (pokemonList) => set({ pokemonList }),
+    setLoading: (loading) => set({ loading }),
+    setError: (error) => set({ error }),
+    setSearchQuery: (searchQuery) => set({ searchQuery }),
+    setSelectedType: (selectedType) => set({ selectedType }),
+    clearFilters: () => set({ searchQuery: '', selectedType: null }),
 
-        return pokemonList.filter((pokemon) => {
-          const containsSearchQuery = pokemon.name
-            .toLowerCase()
-            // If searchQuery is "" it will match all pokemon, since every string includes an empty string
-            .includes(searchQuery.toLowerCase());
+    // Gets the filtered list of pokemon based on the search query and selected type
+    getFilteredPokemon: () => {
+      const { pokemonList, searchQuery, selectedType } = get();
 
-          // If there's a type selected, we check if the pokemon has that type, otherwise (Type not selected) we put true so it doesn't
-          // filter any pokemon based on the type.
-          const matchesSelectedType = selectedType
-            ? pokemon.types.includes(selectedType)
-            : true;
+      return pokemonList.filter((pokemon) => {
+        const containsSearchQuery = pokemon.name
+          .toLowerCase()
+          // If searchQuery is "" it will match all pokemon, since every string includes an empty string
+          .includes(searchQuery.toLowerCase());
 
-          return containsSearchQuery && matchesSelectedType;
-        });
-      },
+        // If there's a type selected, we check if the pokemon has that type, otherwise (Type not selected) we put true so it doesn't
+        // filter any pokemon based on the type.
+        const matchesSelectedType = selectedType
+          ? pokemon.types.includes(selectedType)
+          : true;
 
-      // Fetches the initial list of pokemon
-      fetchInitialPokemon: async (fetchData) => {
-        set({ loading: true, error: null }, false, 'fetchInitialPokemon');
-        try {
-          const data = await fetchData(BATCH_SIZE, 0);
-          set(
-            { pokemonList: data, offset: BATCH_SIZE, loading: false },
-            false,
-            'fetchInitialPokemon:success',
-          );
-        } catch (error) {
-          set(
-            { error: (error as Error).message, loading: false },
-            false,
-            'fetchInitialPokemon:error',
-          );
-        }
-      },
-
-      // Fetches the next batch of 30 pokemon and appends it to the existing list
-      fetchMorePokemon: async (fetchData) => {
-        const { offset, pokemonList } = get();
-        set({ loading: true, error: null }, false, 'fetchMorePokemon');
-        try {
-          const data = await fetchData(BATCH_SIZE, offset);
-          set(
-            {
-              pokemonList: [...pokemonList, ...data],
-              offset: offset + BATCH_SIZE,
-              loading: false,
-            },
-            false,
-            'fetchMorePokemon:success',
-          );
-        } catch (error) {
-          set(
-            { error: (error as Error).message, loading: false },
-            false,
-            'fetchMorePokemon:error',
-          );
-        }
-      },
-
-      // Refreshes the pokemon list by fetching the first batch of 30 pokemon again and replacing the existing list
-      // (Currently does the same thing as fetchInitialPokemon, but it's separated for semantic reasons,
-      // it could be useful in the future if we want to implement a pull-to-refresh functionality for example)
-      refreshPokemonList: async (fetchData) => {
-        set({ loading: true, error: null }, false, 'refreshPokemonList');
-        try {
-          const data = await fetchData(BATCH_SIZE, 0);
-          set(
-            { pokemonList: data, offset: BATCH_SIZE, loading: false },
-            false,
-            'refreshPokemonList:success',
-          );
-        } catch (error) {
-          set(
-            { error: (error as Error).message, loading: false },
-            false,
-            'refreshPokemonList:error',
-          );
-        }
-      },
-    }),
-
-    // The second argument of the devtools function is an optional object with the name of the store,
-    // this is useful for debugging purposes.
-    {
-      name: 'PokemonStore',
+        return containsSearchQuery && matchesSelectedType;
+      });
     },
-  ),
+
+    // Fetches the initial list of pokemon
+    fetchInitialPokemon: async (fetchData) => {
+      set({ loading: true, error: null });
+      try {
+        const data = await fetchData(BATCH_SIZE, 0);
+        set({ pokemonList: data, offset: BATCH_SIZE, loading: false });
+      } catch (error) {
+        set({ error: (error as Error).message, loading: false });
+      }
+    },
+
+    // Fetches the next batch of 30 pokemon and appends it to the existing list
+    fetchMorePokemon: async (fetchData) => {
+      const { offset, pokemonList } = get();
+      set({ loading: true, error: null });
+      try {
+        const data = await fetchData(BATCH_SIZE, offset);
+        set(
+          {
+            pokemonList: [...pokemonList, ...data],
+            offset: offset + BATCH_SIZE,
+            loading: false,
+          },
+          false,
+        );
+      } catch (error) {
+        set({ error: (error as Error).message, loading: false });
+      }
+    },
+
+    // Refreshes the pokemon list by fetching the first batch of 30 pokemon again and replacing the existing list
+    // (Currently does the same thing as fetchInitialPokemon, but it's separated for semantic reasons,
+    // it could be useful in the future if we want to implement a pull-to-refresh functionality for example)
+    refreshPokemonList: async (fetchData) => {
+      set({ loading: true, error: null }, false);
+      try {
+        const data = await fetchData(BATCH_SIZE, 0);
+        set({ pokemonList: data, offset: BATCH_SIZE, loading: false }, false);
+      } catch (error) {
+        set({ error: (error as Error).message, loading: false }, false);
+      }
+    },
+  }),
+
+  // The second argument of the devtools function is an optional object with the name of the store,
+  // this is useful for debugging purposes.
 );
