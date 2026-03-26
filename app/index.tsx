ac021fd0bd810fed80e319b0bf02fece5be690da
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-03-26 13:26:10 +0100

    Update hide index header

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { Stack } from 'expo-router';
 import { ListScreen } from '../src/features/pokemonList/ui/views/listScreen';
 
 export default function HomeScreen() {
-  return <ListScreen />;
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <ListScreen />
+    </>
+  );
 }
