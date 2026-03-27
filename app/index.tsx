753f2c54f8a7b80bec237700b26a7b78bfd9dc65
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-03-27 13:58:08 +0100

    Update listScreen import after rename

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
+import { ListScreen } from '@/src/features/pokemonList/ui/views/ListScreen';
 import { Stack } from 'expo-router';
-import { ListScreen } from '../src/features/pokemonList/ui/views/listScreen';
 
 export default function HomeScreen() {
   return (
