3255aba0db5e19d1d632f85f456718e9d4bb6494
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-04-06 13:52:09 +0200

    listScreen: Fix and code improvement

--- src/features/pokemonList/ui/views/ListScreen.tsx
+++ src/features/pokemonList/ui/views/ListScreen.tsx
@@ -1,46 +1,71 @@
 import { usePokemonListStore } from '@/app/store';
+
 import { brandColors } from '@/constants/colors';
+
 import { BorderRadius, Shadows, Spacing } from '@/constants/theme';
+
 import { SearchBar } from '@/src/features/pokemonList/ui/components/SearchBar';
+
 import { TypeFilter } from '@/src/features/pokemonList/ui/components/TypeFilter';
+
 import { Pokemon } from '@/src/shared/domain/entities/Pokemon';
+
 import { LoadingSpinner } from '@/src/shared/ui/components/loadingSpinner';
+
 import React, { useCallback, useEffect } from 'react';
-import { FlatList, Platform, SafeAreaView } from 'react-native';
+
+import { FlatList, Platform, SafeAreaView, StatusBar } from 'react-native';
+
 import { H1, Paragraph, View, YStack } from 'tamagui';
+
 import { PokemonCard } from '../components/PokemonCard';
 
 const keyExtractor = (item: Pokemon) => item.id.toString();
+
 const ItemSeparator = () => <View height={Spacing.md} />;
 
 export const ListScreen = () => {
-  const state = usePokemonListStore();
-
-  const { loadPokemons, loading, error, hasMore } = usePokemonListStore();
+  const {
+    loadPokemons,
+    loading,
+    error,
+    hasMore,
+    getFilteredPokemon,
+    searchQuery,
+    selectedType,
+    pokemonList,
+  } = usePokemonListStore();
 
   // Get the filtered list, even if no filters are being used, it will show the normal pokemon list
-  const filteredList = state.getFilteredPokemon();
+
+  const filteredList = getFilteredPokemon();
 
   // Effect to load pokemons when loading the app for the first time
+
   useEffect(() => {
-    if (usePokemonListStore.getState().pokemonList.length === 0) {
+    if (pokemonList.length === 0) {
       loadPokemons();
     }
-  }, [loadPokemons]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const renderItem = useCallback(
     ({ item }: { item: Pokemon }) => <PokemonCard pokemon={item} />,
+
     [],
   );
 
   const loadMore = useCallback(() => {
-    if (filteredList.length > 0 && !loading && hasMore) {
+    const isFiltering = searchQuery !== '' || selectedType !== null;
+
+    if (!loading && hasMore) {
       loadPokemons();
     }
-  }, [filteredList.length, loading, hasMore, loadPokemons]);
+  }, [loading, hasMore, searchQuery, selectedType, loadPokemons]);
 
   const renderEmptyComponent = useCallback(() => {
     if (loading) return null;
+
     return (
       <Paragraph fontStyle="italic" color="$color11" textAlign="center">
         {error ? `Error: ${error}` : 'No Pokemons found.'}
@@ -60,7 +85,9 @@ export const ListScreen = () => {
         paddingHorizontal={Spacing.md}
       >
         <YStack
-          paddingTop={Platform.OS === 'android' ? 40 : Spacing.lg}
+          paddingTop={
+            Platform.OS === 'android' ? StatusBar.currentHeight : Spacing.lg
+          }
           paddingBottom={Spacing.lg}
           borderBottomWidth={1}
           borderBottomColor={brandColors.borderLight}
@@ -68,8 +95,10 @@ export const ListScreen = () => {
         >
           <H1 color={brandColors.primaryRed}>Pokédex</H1>
         </YStack>
+
         <YStack flex={1}>
           {/* Filters and search container */}
+
           <YStack
             marginBottom={Spacing.md}
             padding={Spacing.md}
@@ -78,9 +107,12 @@ export const ListScreen = () => {
             {...Shadows.base}
           >
             <SearchBar />
+
             <TypeFilter />
           </YStack>
+
           {/* List container */}
+
           <YStack
             flex={1}
             backgroundColor={brandColors.surfaceLight}
