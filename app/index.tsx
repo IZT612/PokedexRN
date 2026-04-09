f89386cf118aed77d3f767a5939aabd63a84c827
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-04-09 13:40:15 +0200

    Temporarily replace list view with detail view

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import { ListScreen } from '@/src/features/pokemonList/ui/views/ListScreen';
+// import { ListScreen } from '@/src/features/pokemonList/ui/views/ListScreen';
+import { DetailScreen } from '@/src/features/pokemonDetail/ui/views/PokemonDetailScreen';
 import { Stack } from 'expo-router';
 
 export default function HomeScreen() {
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
-      <ListScreen />
+      {/* Add an onBack function too to see the go back button */}
+      <DetailScreen id={1} onBack={() => {}} />
     </>
   );
 }
