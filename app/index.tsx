44e8927ce427e6afe2329ba9e146fe0f1cc5ee6a
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-04-10 01:28:31 +0200

    Readd ListScreen as default screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import PokemonDetailScreen from '@/src/features/pokemonDetail/ui/views/PokemonDetailScreen';
+import { ListScreen } from '@/src/features/pokemonList/ui/views/ListScreen';
 import { Stack } from 'expo-router';
 
 export default function HomeScreen() {
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
-      {/* Add an onBack function too to see the go back button */}
-      <PokemonDetailScreen id={1} onBack={() => {}} />
+      <ListScreen />
     </>
   );
 }
