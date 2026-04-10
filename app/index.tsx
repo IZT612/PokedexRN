da6de3a98060a9877dc8379cd5e9f95e54d9d578
Iván Zamora Torres <ivan.zamora@iesnervion.es>
2026-04-10 13:50:16 +0200

    app/index.ts: Redirect to tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import { ListScreen } from '@/src/features/pokemonList/ui/views/ListScreen';
-import { Stack } from 'expo-router';
+import { Redirect } from 'expo-router';
 
 export default function HomeScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ headerShown: false }} />
-      <ListScreen />
-    </>
-  );
+  return <Redirect href="/tabs" />;
 }
