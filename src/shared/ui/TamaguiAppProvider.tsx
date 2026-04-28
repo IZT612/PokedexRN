55d0dadab02c0170e4971bbd8d9d6d12219166ff
IZT612 <ivan.zamora@iesnervion.es>
2026-04-28 11:22:12 +0200

    Add Tamagui runtime configuration

--- src/shared/ui/TamaguiAppProvider.tsx
+++ src/shared/ui/TamaguiAppProvider.tsx
@@ -1,8 +1,14 @@
-// @ts-nocheck
 import React, { PropsWithChildren } from "react";
+import { TamaguiProvider } from "tamagui";
+
+import { tamaguiConfig } from "../../../tamagui.config";
 
 type TamaguiAppProviderProps = PropsWithChildren<{}>;
 
 export function TamaguiAppProvider({ children }: TamaguiAppProviderProps) {
-  return <>{children}</>;
+  return (
+    <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
+      {children}
+    </TamaguiProvider>
+  );
 }
